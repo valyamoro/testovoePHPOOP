0923dcdd55da22c85e5465abd9713ac08a918d7b
kutlumbek <Moralek228@gmail.com>
2024-01-03 06:49:59 +0300

    dev#display_products feat (Теперь товары выводятся на главной странице)

--- views/home.php
+++ views/home.php
@@ -1,2 +1,11 @@
-<h1>Home</h1>
-<h3>welcome <?php echo $name ?> </h3>
+<?php foreach ($products as $product): ?>
+    <a href="/editProduct?id=<?php echo $product['id']; ?>"><?php echo $product['name']; ?></a> <br>
+    Описание: <?php echo $product['description']?> <br>
+    Цена: <?php echo $product['price']; ?> <br>
+    <label>
+        <input type='number' value='<?php echo $product['price']; ?>' data-id=' <?php echo $product['id'] ?>' class='price-input'>
+    </label>
+    <br>
+    <img src="<?php echo $product['image_path']; ?>" alt="Изображение">
+<br><br><br>
+<?php endforeach; ?>
