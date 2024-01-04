9afe1b851111837988a30faf906cc883c4dabb1b
kutlumbek <Moralek228@gmail.com>
2024-01-04 07:39:19 +0300

    dev#edit_product fix (Теперь старые изображения удаляются)

--- models/EditProductModel.php
+++ models/EditProductModel.php
@@ -19,4 +19,24 @@ class EditProductModel extends ProductModel
 
         return (bool)$result;
     }
+
+    public function getImageById(int $id): array
+    {
+        $query = 'SELECT image_path FROM ' . static::TABLE_NAME . ' WHERE id=? LIMIT 1';
+
+        $this->builder->prepare($query)->execute([$id]);
+
+        return $this->builder->fetch();
+    }
+
+    public function uploadImage(array $data): string
+    {
+        // Потом сделать так чтобы папка автоматически создавалась при заходе на сайт.
+        $filePath = __DIR__ . '\..\uploads\\' . \uniqid() . $data['name'];
+
+        \move_uploaded_file($data['tmp_name'], $filePath);
+
+        return '\..\\' . \strstr($filePath, 'uploads');
+    }
+
 }
--- models/ProductModel.php
+++ models/ProductModel.php
@@ -9,11 +9,13 @@ class ProductModel extends Model
 {
     const TABLE_NAME = 'products';
 
-//    public string $imagePath;
+    public string $imagePath;
     public int $id;
     public string $name;
-    public int $price;
+    public string|int $price;
     public string $description;
+    public string $createdAt;
+    public string $updatedAt;
 
     public function __construct(
         protected PDODriver $builder,
